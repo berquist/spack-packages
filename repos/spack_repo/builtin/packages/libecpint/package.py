e74b04f8616f3e9fc65efe31cced4e0867c436d7
Eric Berquist <eric.berquist@gmail.com>
2025-11-14 14:51:10 -0500

    Backport removal of unnecessary dependency on C compiler

--- repos/spack_repo/builtin/packages/libecpint/package.py
+++ repos/spack_repo/builtin/packages/libecpint/package.py
@@ -27,10 +27,14 @@ class Libecpint(CMakePackage):
     version("1.0.1", sha256="245b89fe8cb0a92cbbb79c811b48cb15fcfc937389df89387466f1bf76a096bf")
     version("1.0.0", sha256="47d741cc48a543ef9c85483cb2d5cd1c9f6677fa7e9920886d083b3c25232379")
 
-    depends_on("cxx", type="build")  # generated
+    depends_on("cxx", type="build")
 
+    depends_on("cmake@3.12:", type="build")
     depends_on("pugixml")
     depends_on("googletest")
+
+    # Backport of https://github.com/robashaw/libecpint/pull/67
+    patch("67-remove-c-compiler-dependency.diff", level=0)
 
     def cmake_args(self):
         args = ["-DBUILD_SHARED_LIBS=ON"]
